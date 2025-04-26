5507f54229df6bd11e51c7f6efeb5fe2aa95e42a
POTOBAWI <brunopotobawi80@gmail.com>
2025-04-26 14:35:26 +0000

    resolution des problemes avec sign up et login

--- puddle/settings.py
+++ puddle/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5q)#-9v7bg3trz4oml^&poqbtmwx35k#rsl^_n%inqz*7a4ay!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com','127.0.0.1']
 LOGIN_URL="/login/"
 LOGIN_REDIRECT_URL="/"
 LOGOUT_REDIRECT_URL="/"
