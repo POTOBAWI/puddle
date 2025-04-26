8a8e235dbc26d3abc2918cfd9943d740f2b13899
POTOBAWI <brunopotobawi80@gmail.com>
2025-04-26 10:08:33 +0000

    ajout render.yaml et ajout d autres dependances pour mettre en ligne le site

--- puddle/settings.py
+++ puddle/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5q)#-9v7bg3trz4oml^&poqbtmwx35k#rsl^_n%inqz*7a4ay!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["POTOBAWI.pythonanywhere.com"]
+ALLOWED_HOSTS = ['.onrender.com']
 LOGIN_URL="/login/"
 LOGIN_REDIRECT_URL="/"
 LOGOUT_REDIRECT_URL="/"
