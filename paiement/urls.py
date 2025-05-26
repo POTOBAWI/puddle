bebb7f37becdd9f5fd347f388a24316709548226
POTOBAWI <brunopotobawi80@gmail.com>
2025-05-26 17:00:27 +0000

    suppression de paiement

--- paiement/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('payer/', views.payer, name='payer'),
-    path('success/', views.paiement_success, name='success'),
-    path('webhook/', views.webhook, name='webhook'),
-]
